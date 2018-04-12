b53422bd40ddd05fa21303449e30fd4e7f623d01
appled <appled@appledtekiiMac.local>
2018-04-12 17:19:34 +0800

    Initial Commit

---
+++ MMFFmpeg/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MMFFmpeg
+//
+//  Created by appled on 2018/4/12.
+//  Copyright © 2018年 appled. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MMFFmpeg/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MMFFmpeg
+//
+//  Created by appled on 2018/4/12.
+//  Copyright © 2018年 appled. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
